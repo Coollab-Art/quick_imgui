a025b954337199cfd6b3d02ebea88fdf06a085cc
Jules Fouchy <jules.fouchy@ntymail.com>
2024-08-20 16:21:59 +0200

    ✨ Added AverageTime to measure performance

--- include/quick_imgui/quick_imgui.hpp
+++ include/quick_imgui/quick_imgui.hpp
@@ -1,9 +1,26 @@
 #pragma once
 #include <imgui/imgui.h>
+#include <chrono>
 #include <functional>
+#include <vector>
 
 namespace quick_imgui {
 
 void loop(const char* window_name, std::function<void()> callback);
+
+/// A convenience class to measure the performance of a block of code, and display it with ImGui
+/// Press space to pause the graphics
+class AverageTime {
+public:
+    void start();
+    void end();
+    auto average_time_ms() const -> float;
+    void imgui_plot() const;
+
+private:
+    std::chrono::steady_clock::time_point _start_time{};
+    std::vector<float>                    _times{};
+    mutable std::vector<float>            _times_when_paused{};
+};
 
 } // namespace quick_imgui
