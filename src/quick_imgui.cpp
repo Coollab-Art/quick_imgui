3dc48cb1e031e485913d0a354c766bfcbb87e534
dsmtE <desmet.enguerrand@gmail.com>
2025-03-11 11:08:15 +0100

    🚨 add NOLINT

--- src/quick_imgui.cpp
+++ src/quick_imgui.cpp
@@ -18,28 +18,28 @@ void loop(const char* window_name, Callbacks callbacks) {
     // It will work as the local copy of callbacks will live during this loop function (same lifetime as the window)
     glfwSetWindowUserPointer(window, &callbacks);
 
-    glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+    glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) { //NOLINT(bugprone-easily-swappable-parameters)
         CallbackUserDataFromWindow(window).key_callback(key, scancode, action, mods);
         ImGui_ImplGlfw_KeyCallback(window, key, scancode, action, mods);
     });
 
-    glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
+    glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) { //NOLINT(bugprone-easily-swappable-parameters)
         CallbackUserDataFromWindow(window).mouse_button_callback(button, action, mods);
         ImGui_ImplGlfw_MouseButtonCallback(window, button, action, mods);
     });
 
-    glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xpos, double ypos) {
+    glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xpos, double ypos) { //NOLINT(bugprone-easily-swappable-parameters)
         CallbackUserDataFromWindow(window).cursor_position_callback(xpos, ypos);
         ImGui_ImplGlfw_CursorPosCallback(window, xpos, ypos);
     });
 
 
-    glfwSetScrollCallback(window, [](GLFWwindow* window, double xoffset, double yoffset) {
+    glfwSetScrollCallback(window, [](GLFWwindow* window, double xoffset, double yoffset) { //NOLINT(bugprone-easily-swappable-parameters)
         CallbackUserDataFromWindow(window).scroll_callback(xoffset, yoffset);
         ImGui_ImplGlfw_ScrollCallback(window, xoffset, yoffset);
     });
 
-    glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+    glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) { //NOLINT(bugprone-easily-swappable-parameters)
         CallbackUserDataFromWindow(window).size_callback(width, height);
     });
 
